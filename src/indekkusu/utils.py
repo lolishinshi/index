a1a9e715f1158a341fdcf4dd2aa281ef7209adf2
Aloxaf <aloxafx@gmail.com>
2024-06-07 00:34:09 +0800

    update

--- src/indekkusu/utils.py
+++ src/indekkusu/utils.py
@@ -20,12 +20,19 @@ def load_image(image: Path | str | bytes) -> MatLike | None:
     return img
 
 
-def resize_image(img: MatLike, width: int = 1080, height: int = 1920) -> MatLike:
+def resize_image(
+    img: MatLike, width: int = 1080, height: int = 1920, min_width: int = 400
+) -> MatLike:
     """
     将图片按照宽度等比例缩放
     """
     h, w = img.shape[:2]
     if w <= width and h <= height:
+        if w < min_width:
+            scale = min_width / w
+            return cv2.resize(
+                img, (0, 0), fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC
+            )
         return img
     else:
         scale = min(width / w, height / h)
